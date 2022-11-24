db7dcd1ceff8b6bb3113048dfe5cc5ca419591e9
dryego barbosa <dryego.lisboa@gmail.com>
2022-11-24 09:30:54 -0300

    Implementação do controlador e rotas (remover, adicionar,adicionarComIndice).

--- 02/src/controladores/jogadas.js
+++ 02/src/controladores/jogadas.js
@@ -5,13 +5,46 @@ const jogadas = (req, res) => {
     if (i == jogadores.length) {
         i = 0;
     }
-    let jogadorDaVez = jogadores[i];
+    //let jogadorDaVez = jogadores[i];
     res.send(`É a vez de ${jogadores[i]} jogar!`)
     i++;
-
-
 }
+
+const removerJogador = (req, res) => {
+    const { indice } = req.query;
+    let i = Number(indice);
+    jogadores.splice(i, 1);
+    if (i > jogadores.length) {
+        res.send(`Não existe jogador no índice informado (${i}) para ser removido.`)
+    } else {
+        res.send(`${jogadores}`);
+    }
+};
+
+const adicionarJogador = (req, res) => {
+    const { nome } = req.query;
+    jogadores.push(nome);
+    res.send(`${jogadores}`);
+};
+
+const adicionarJogadorIndice = (req, res) => {
+    const { nome, indice } = req.query;
+    const nomeFormatado = (nome) => {
+        const primeiraLetra = nome.slice(0, 1).toUpperCase()
+        const restopalavra = nome.slice(1).toLowerCase()
+        return `${primeiraLetra + restopalavra}`;
+    }
+    jogadores.splice(indice, 0, nomeFormatado(nome));
+    if (indice > jogadores.length) {
+        res.send(`O índice informado (${indice}) não existe no array. Novo jogador não adicionado.`)
+    } else {
+        res.send(`${jogadores}`);
+    }
+};
 
 module.exports = {
     jogadas,
+    removerJogador,
+    adicionarJogador,
+    adicionarJogadorIndice
 }
--- 02/src/index.js
+++ 02/src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { jogadas } = require('./controladores/jogadas');
+const { jogadas, removerJogador, adicionarJogador, adicionarJogadorIndice } = require('./controladores/jogadas');
 
 const app = express();
 
+//rotas
 app.get('/', jogadas);
+app.get('/remover', removerJogador);
+app.get('/adicionar', adicionarJogador);
+app.get('/adicionarnoindice', adicionarJogadorIndice);
 
 
 app.listen(8000, console.log('Servidor Inicializado...'));
